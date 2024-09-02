f985449ffa853b138915c8b5f16091fffdf00851
ltxhhz <ltxhhz@qq.com>
2024-09-02 11:44:11 +0800

    9.2 重构简化代码

--- src/强智教务/新疆师范大学/parser.js
+++ src/强智教务/新疆师范大学/parser.js
@@ -11,121 +11,106 @@ function scheduleHtmlParser(html) {
     config = JSON.parse($('#ltxhhzConfig').text())
   } catch (error) {}
   try {
-    $('tbody').children().each(function (i, el) {
-      if (i) { //除去第一行
-        const num = [] //那一节课到哪一节课
-        let brk = false
-        $(el).children().each(function (i1, el1) { //tr
-          console.log(`第${i}行 第${i1}列`);
-          if (brk) return
-          if (i1 == 0) { //th
-            let inner = el1.childNodes[0]
-            if (inner.data.includes('备注')) {
-              brk = true
-              return
-            }
-            brk = false
-            num.push(...Array.from(inner.data.trim().matchAll(/\d+/g)).map(e => +e))
-          } else { //td
-            $(this).children().each((i2, el2) => { //格子
-              const none = new RegExp(/display\s?:\s?none/)
-              if (el2.name == 'div' && !(el2.attribs && (el2.attribs.type == 'hidden' || none.test(el2.attribs.style)))) { //显示的格子
-                let cls = {
-                  name: ''
-                }
-                el2.childNodes.forEach((node, index) => { //格子里每一行
-                  if (node.type == 'text') {
-                    /**@type {string} */
-                    const str = node.data.trim()
-                    if (!str) return
-                    if (/^\-+$/.test(str)) { //分割线
-                      result.push(cls)
-                      cls = {
-                        name: ''
-                      }
+    $('tbody')
+      .children()
+      .each(function (i, el) {
+        if (i) {
+          //除去第一行
+          const num = [] //哪一节课到哪一节课
+          const children1 = $(el).children()
+          for (let i1 = 0; i1 < children1.length; i1++) {
+            //tr
+            const el1 = children1.get(i1)
+            console.log(`第${i}行 第${i1}列`)
+            if (i1 === 0) {
+              //th
+              let inner = $(el1).text().trim()
+              if (inner.includes('备注')) {
+                break
+              }
+              num.push(...Array.from(inner.matchAll(/\d+/g)).map(e => +e))
+            } else {
+              //td
+              let cls = {
+                name: '',
+                day: i1,
+                sections: [2 * i - 1, 2 * i]
+              }
+              //td
+              $('.kbcontent', el1)
+                .children()
+                .each(function (i2, el2) {
+                  const str = $(el2).text().trim()
+                  if (!cls.name) {
+                    if (/^\-+$/.test(el2.prev.data.trim())) {
                       return
                     }
-                    if (config.isTeacher) {
-                      if (/\-.+班/.test(str)) {
-                        cls.teacher = str
-                      } else if (/学时.\d/.test(str)) {
-                        cls.name += `|${str.match(/学时.(\d+)/)[1]}学时`
-                      } else if (/\[[\d\-]+\]/.test(str)) {
-                        //匹配第几节课 不管
-                      } else {
-                        cls.name += cls.name ? `|${str}` : str
-                      }
-                    } else {
-                      cls.name += cls.name ? `|${str}` : str
-                    }
-
-                    cls.day = i1
-                    cls.sections = num.map(sec => ({
-                      section: sec
-                    }))
+                    cls.name = el2.prev.data.trim()
                   } else {
-                    if (node.attribs && none.test(node.attribs.style)) return
-                    if (node.name == 'font') {
-                      if (node.attribs && node.attribs.title && node.attribs.title.includes('教师')) {
-                        cls.teacher = $(node).text().replace(config.delTitle ? /其他|副?教授|讲师|\(.+?\)|（.+?）/g : '', '').trim()
-                      } else if (node.attribs && node.attribs.title && node.attribs.title.includes('周次')) {
-                        const str = /(\d+[\s\-]*\d+|[\d\s,]+).*周/.exec($(node).text().trim()),
-                          str1 = /([\d\s,\-]+).*周/.exec($(node).text().trim())
-                        if (str1[1].includes('-') && str1[1].includes(',')) {
-                          cls.weeks = []
-                          str1[1].split(',').map(v => {
-                            if (v.includes('-')) {
-                              const arr = v.split('-'),
-                                arr1 = Array(arr[1] - arr[0] + 1).fill().map((v, i) => +i + +arr[0].trim())
-                              if (str[0].includes('单周')) {
-                                return arr1.filter(v => v & 1)
-                              } else if (str[0].includes('双周')) {
-                                return arr1.filter(v => !(v & 1))
-                              } else {
-                                return arr1
-                              }
-                            } else {
-                              return +v.trim()
+                    if (el2.attribs && el2.attribs.title && el2.attribs.title.includes('教师')) {
+                      if (config.delTitle) {
+                        cls.teacher = str.replace(config.delTitle ? /(其他|副?教授|讲师|农艺师|\(.+?\)|（.+?）)$/g : '', '').trim()
+                      } else {
+                        cls.teacher = str
+                      }
+                    } else if (el2.attribs && el2.attribs.title && el2.attribs.title.includes('教室')) {
+                      cls.position = str
+                    } else if (el2.attribs && el2.attribs.title && el2.attribs.title.includes('周次')) {
+                      cls.weeks = []
+                      const weekStr = str.match(/([\d-,]+)\((.?周)\)/)
+                      weekStr[1]
+                        .split(',')
+                        .map(w => {
+                          if (w.includes('-')) {
+                            const arr = w.split('-')
+                            const arr1 = Array(arr[1] - arr[0] + 1)
+                              .fill()
+                              .map((v, i) => +i + +arr[0].trim())
+                            if (weekStr[2] === '单周') {
+                              return arr1.filter(v => v & 1)
+                            } else if (weekStr[2] === '双周') {
+                              return arr1.filter(v => !(v & 1))
                             }
-                          }).forEach(e => {
-                            if (typeof e == 'string' || typeof e == 'number') {
-                              cls.weeks.push(e)
-                            } else {
-                              cls.weeks.push(...e)
-                            }
-                          })
-                        } else if (str[1].includes('-')) {
-                          const arr = str[1].split('-'),
-                            arr1 = Array(arr[1] - arr[0] + 1).fill().map((v, i) => +i + +arr[0].trim())
-                          if (str[0].includes('单周')) {
-                            cls.weeks = arr1.filter(v => v & 1)
-                          } else if (str[0].includes('双周')) {
-                            cls.weeks = arr1.filter(v => !(v & 1))
+                            return arr1
                           } else {
-                            cls.weeks = arr1
+                            return +w.trim()
                           }
-                        } else {
-                          cls.weeks = str[1].split(',').map(v => +v.trim())
-                        }
-                      } else if (node.attribs && node.attribs.title && node.attribs.title.includes('教室')) {
-                        cls.position = $(node).text().trim()
+                        })
+                        .forEach(w => {
+                          if (Array.isArray(w)) {
+                            cls.weeks.push(...w)
+                          } else {
+                            cls.weeks.push(w)
+                          }
+                        })
+                    } else if (el2.next && el2.next.type === 'text' && /^\-+$/.test(el2.next.data)) {
+                      //分割线
+                      if (!cls.weeks) {
+                        throw new Error('未匹配周次')
+                      }
+                      result.push(cls)
+                      cls = {
+                        name: '',
+                        day: i1,
+                        sections: [2 * i - 1, 2 * i]
                       }
                     }
                   }
                 })
-                if (cls.name) {
-                  result.push(cls)
+              if (cls.name) {
+                if (!cls.weeks) {
+                  throw new Error('未匹配周次')
                 }
-                console.info(cls)
+                result.push(cls)
               }
-            })
+              console.info(cls)
+            }
           }
-        })
-      }
-    })
+        }
+      })
     console.info(result)
     return result
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
 }
--- src/强智教务/新疆师范大学/provider.js
+++ src/强智教务/新疆师范大学/provider.js
@@ -1,4 +1,4 @@
-async function scheduleHtmlProvider(iframeContent = "", frameContent = "", dom = document) {
+async function scheduleHtmlProvider(iframeContent = '', frameContent = '', dom = document) {
   // 内嵌loadTool工具，传入工具名即可引用公共工具函数(暂未确定公共函数，后续会开放)
   await loadTool('AIScheduleTools')
   if (typeof AIScheduleAlert == 'undefined') {
@@ -22,61 +22,27 @@ async function scheduleHtmlProvider(iframeContent = "", frameContent = "", dom =
       }
     }
     if (!frame || (frameDom.title !== '学期理论课表' && frameDom.title !== '个人课表信息')) {
-      await AIScheduleAlert('在"学期理论课表/个人课表信息(教师端)"页面出现后，选择当前学期的课表，然后点击一键导入\n有问题请联系qq/wx:1270897967')
+      await AIScheduleAlert('在"学期理论课表(学生端)/个人课表信息(教师端)"页面出现后，选择当前学期的课表，然后点击一键导入\n有问题请联系qq/wx:1270897967')
       return 'do not continue'
-      /* //支持异步时使用 维持现状能用就行。。
-      center.contentDocument.querySelector("body > div.personal-center.student-personal-center > div.personal-right-item > div.personal-function.personal-common.personal-height > ul > div:nth-child(1)").click()
-      // center.contentWindow.kjcdShow('NEW_XSD_PYGL', 'NEW_XSD_PYGL_WDKB', 'NEW_XSD_PYGL_WDKB_XQLLKB', '/xskb/xskb_list.do', '学期理论课表')
-      alert('在"学期理论课表"页面出现后，选择当前学期的课表，然后点击一键导入')
-      setTimeout(() => {
-        for (const node of content.children) {
-          !center && (center = node)
-          if (node.style.display != 'none') {
-            frame = node
-            frameDom = node.contentDocument
-            break
-          }
-        }
-      }, 1500);
-      return new Promise((resolve, reject) => {
-        let btn = dom.createElement('div')
-        btn.className = 'button el-button'
-        btn.innerText = '一键导入'
-        btn.style.position = 'fixed'
-        btn.style.top = 0
-        btn.style.left = 0
-        btn.style.width = innerWidth+'px'
-        btn.style.height = innerHeight * 0.1+'px'
-        btn.onclick = function (e) {
-          try {
-            resolve(frameDom.getElementById('timetable').outerHTML)
-          } catch (error) {
-            alert('获取失败，请检查课表页面是否打开')
-          }
-        }
-        debugger
-        dom.body.appendChild(btn)
-      }).then(e => {
-        alert("如果生成的课表中“午休”和“晚休”的位置不对，请到设置中的“课程表节数和时间设置”里的“一天课节数”修改早中晚的课数。\np.s.如果记不住请截图")
-        return e
-      }) */
     }
     let configDiv = frameDom.getElementById('ltxhhzConfig')
     if (!configDiv) {
-      configDiv = frameDom.getElementById('timetable').appendChild((() => {
-        const div = document.createElement('div')
-        div.id = 'ltxhhzConfig'
-        div.style.display = 'none'
-        return div
-      })())
+      configDiv = frameDom.getElementById('timetable').appendChild(
+        (() => {
+          const div = document.createElement('div')
+          div.id = 'ltxhhzConfig'
+          div.style.display = 'none'
+          return div
+        })()
+      )
     }
     const configObj = {}
-    configObj.isTeacher = document.querySelector(".userInfo span").innerText === '教师'
+    configObj.isTeacher = document.querySelector('.userInfo span').innerText === '教师'
     if (!configObj.isTeacher) {
       /** @type {boolean} */
       const userConfirm = await AIScheduleConfirm({
         titleText: '提示',
-        contentText: '是否隐藏老师的职称信息(“教授”、“导师”等)？',
+        contentText: '是否隐藏老师的职称信息(“教授”、“导师”等)？\n点击后请等待一会',
         cancelText: '保留',
         confirmText: '隐藏'
       })
@@ -87,39 +53,16 @@ async function scheduleHtmlProvider(iframeContent = "", frameContent = "", dom =
 
     configDiv.innerText = JSON.stringify(configObj)
     // await AIScheduleAlert("如果生成的课表中“午休”和“晚休”的位置不对，请到设置中的“课程表节数和时间设置”里的“一天课节数”修改早中晚的课数。\np.s.如果记不住请截图\n有问题请联系qq:1270897967")
-    const timetable = frameDom.getElementById('timetable')
-    moveNonTextElementsAfterFontElements(Array.from(timetable.querySelectorAll('td>div>font')))
+    const timetable = frameDom.getElementById('timetable').cloneNode(true)
+    checkHidden(timetable.querySelector('tbody'))
     return timetable.outerHTML
   }
 }
-/**
- * @param {HTMLFontElement[]} fontElements 
- */
-function moveNonTextElementsAfterFontElements(fontElements) {
-  for (let i = 0; i < fontElements.length; i++) {
-    const fontElement = fontElements[i];
 
-    const childNodes = fontElement.childNodes;
-    const nextElement = fontElement.nextSibling
-
-    if (fontElement.style.display === 'none' || !Array.from(childNodes).some(node => node.nodeType !== Node.TEXT_NODE && node.tagName !== 'BR')) {
-      continue;
-    }
-
-    const parentElement = fontElement.parentElement;
-    let skip = true
-    for (let j = 0; j < childNodes.length;) {
-      const node = childNodes[j];
-      if (node.nodeType === Node.TEXT_NODE) {
-        if (skip) {
-          skip = false
-          j++
-        } else {
-          parentElement.insertBefore(node, nextElement);
-        }
-      } else {
-        parentElement.insertBefore(node, nextElement);
-      }
-    }
+function checkHidden(element) {
+  if (element.type === 'hidden' || element.style.display === 'none') {
+    element.remove()
+  } else {
+    Array.from(element.children).forEach(checkHidden)
   }
 }
