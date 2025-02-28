fd55ed872a1339ff941f27df6de0949799698e9a
ltxhhz <ltxhhz@aliyun.com>
2025-02-28 15:46:30 +0800

    2.28 添加样例

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
 let str = fs.readFileSync('./providerResult.txt').toString()
 const cheerio = require('cheerio');
-eval(fs.readFileSync('./parser.js').toString())
+eval(fs.readFileSync('./src/强智教务/新疆师范大学/parser.js').toString())
 console.log(scheduleHtmlParser(str))
